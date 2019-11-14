b12fcdc8f036a3c388c26dbd1075868d2cb8d15f
mladjo97 <mldnmilosevic@gmail.com>
2019-11-14 16:20:17 +0100

    jwt token

--- services/reviews-api/create.js
+++ services/reviews-api/create.js
@@ -1,23 +1,26 @@
 import uuid from 'uuid/v4';
 import fetch from 'node-fetch';
 import jwt from 'jsonwebtoken';
-//import jwkToPem from 'jwk-to-pem';
+import jwkToPem from 'jwk-to-pem';
 
 import config from '../../config';
 import * as dynamoDb from '../../libs/dynamodb.lib';
 import { ok, internalServerError, badRequest } from '../../libs/response.lib';
 
 export const main = async (event) => {
+
+  // kad nema dobar token u auth headeru onda uopste ne prodje apigateway
+  // ali ovde ispod sam eksperimentisao sa parsiranjem tokena, jer je u
+  // aws-u to malo drugacije nego inace
   const token = event.headers.Authorization;
 
-  // when there is no auth token, it doesn't get through apigateway
   if (!token)
     return badRequest('No auth token was found.');
 
   try {
-    const pem = await getPem(config.region, config.userPoolId);
-    const decoded = await jwt.verify(token, pem, { algorithms: ['RS256'] });
-
+    const pKey = await getPem(config.region, config.userPoolId);
+    const decoded = await jwt.verify(token, pKey, { algorithms: ['RS256'] });
+    // decoded sadrzi sad info o username, email itd.
     if (!decoded)
       return badRequest('Not a valid token.');
   } catch (err) {
@@ -25,6 +28,7 @@ export const main = async (event) => {
     return internalServerError(err.message);
   }
 
+  // glavna logika lambde
   const { review, rating } = JSON.parse(event.body);
 
   if (!review || !rating)
@@ -51,5 +55,5 @@ export const main = async (event) => {
 
 const getPem = async (region, userPoolId) => {
   const jwkUrl = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
-  return await fetch(jwkUrl).then(res => console.log(res.json()));
-}
+  return await fetch(jwkUrl).then(data => data.json()).then(json => jwkToPem(json.keys[1])).then(pem => pem);
+};
